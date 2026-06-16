395e9199935f4670deeae2566ad6704361b6db7c
Mirko Stefanini <mirko.stefanini@proximagroup.eu>
2026-06-16 12:49:13 +0200

    add PWA icons

--- dnd_app/vite.config.ts
+++ dnd_app/vite.config.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'D&D Companion',
+        short_name: 'D&D',
+        description: 'Gestisci i tuoi personaggi D&D',
+        theme_color: '#0f0f13',
+        background_color: '#0f0f13',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
